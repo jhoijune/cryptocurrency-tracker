e5e03bb072ff0a9db96256d9ca25b728be59843d
jhoijune <jhoijune@gmail.com>
2020-01-19 16:12:52 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Android-based cryptocurrency tracking applications
+
+
+TODO:
+
+- [ ] 가상화폐 이미지 취합g
+
+DONE: 
+
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+const StackNavigator = createStackNavigator({});
+
+export default createAppContainer(StackNavigator);
